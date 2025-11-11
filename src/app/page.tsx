128b046514fed5e60738d4ae59790c846a125d24
baranecp <baranec.dev@gmail.com>
2025-11-11 17:02:04 +0100

    Add global flashlight cursor effect using GSAP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import FlashlightWrapper from "./components/ui/FlashlightWrapper";
 import LeftSocial from "./components/ui/LeftSocial";
 import RightSocial from "./components/ui/RigthSocial";
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-background px-12 py-8'>
-      <Navbar />
-      <main className='mx-[120px] px-[150px]'>
-        <LeftSocial />
-        <RightSocial />
-        <Hero />
-      </main>
-    </div>
+    <FlashlightWrapper>
+      <div className='min-h-screen px-12 py-8'>
+        <Navbar />
+        <main className='mx-[120px] px-[150px]'>
+          <LeftSocial />
+          <RightSocial />
+          <Hero />
+        </main>
+      </div>
+    </FlashlightWrapper>
   );
 }
