fab00e7598aa2af9a7bd052b3a684af570dad0b3
baranecp <baranec.dev@gmail.com>
2025-11-11 16:21:31 +0100

    Add Hero section and Navbar

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,27 @@
+export default function Hero() {
+  return (
+    <section className='min-h-screen flex justify-center items-start pt-[200px]'>
+      <div className='flex flex-col gap-4'>
+        <p className='hero-intro text-[#64ffda] font-mono'>Hi, my name is</p>
+        <div>
+          <h2 className='hero-name text-[clamp(2.5rem,8vw,5rem)] font-sans font-bold text-lightSlate leading-tight'>
+            Peter Baranec.
+          </h2>
+          <h3 className='hero-desc text-[clamp(2.5rem,8vw,4rem)] text-slate font-sans font-bold leading-tight'>
+            I build things for the web.
+          </h3>
+        </div>
+        <p className='max-w-lg text-muted'>
+          I’m a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building accessible, human-centered products.
+        </p>
+        <a
+          href=''
+          className='mt-6 border rounded px-5 py-3 border-accent text-accent font-mono text-[1rem] w-max'>
+          Get in touch!
+        </a>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import LeftSocial from "./components/ui/LeftSocial";
+import RightSocial from "./components/ui/RigthSocial";
+
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-background'>
-      <main></main>
+    <div className='min-h-screen bg-background px-12 py-8'>
+      <Navbar />
+      <main className='mx-[120px] px-[150px]'>
+        <LeftSocial />
+        <RightSocial />
+        <Hero />
+      </main>
     </div>
   );
 }
